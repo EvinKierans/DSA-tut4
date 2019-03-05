f661de6971a9e6f483d549c576484fcf198fa075
Evin Kierans <44497184+EvinKierans@users.noreply.github.com>
2019-03-05 11:26:29 +0000

    Add files via upload

---
+++ src 2/dsaii/bst/ProperLinkedAVLBinaryTree.java
@@ -0,0 +1,21 @@
+package dsaii.bst;
+
+import dsaii.common.Position;
+
+public class ProperLinkedAVLBinaryTree<T> extends ProperLinkedBSTBinaryTree<T> {
+    public Position<T> singleLeft(Position<T> _a, Position<T> _b, Position<T> _c) {
+        return null;
+    }
+
+    public Position<T> singleRight(Position<T> _a, Position<T> _b, Position<T> _c) {
+        return null;
+    }
+
+    public Position<T> doubleLeftRight(Position<T> a, Position<T> b, Position<T> c) {
+        return null;
+    }
+
+    public Position<T> doubleRightLeft(Position<T> a, Position<T> b, Position<T> c) {
+        return null;
+    }
+}
